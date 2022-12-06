5dc4cbbdeb2491eff8ae4526d936c846988f640f
ftagliaca <filippotagliacarne@gmail.com>
2022-12-06 08:13:59 +0100

    Day 06

---
+++ day06/part1.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    line = s.strip()
+    len_marker = 4
+    for i, c in enumerate(line[:-len_marker]):
+        if len(set(line[i:i+len_marker])) == len_marker:
+            return i + len_marker
+    return len(line)
+
+
+INPUT_S = '''\
+
+'''
+EXPECTED = 1
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        ('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7),
+        ('bvwbjplbgvbhsrlpgdmjqwftvncz', 5),
+        ('nppdvjthqldpwncqszvftbrmjlhg', 6),
+        ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10),
+        ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
